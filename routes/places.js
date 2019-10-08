525c72227766471475712f8481e14dae3d55cb39
Carlos Garcia <carlosgarlem@gmail.com>
2019-10-08 00:13:05 -0600

    api validations

--- routes/places.js
+++ routes/places.js
@@ -29,11 +29,31 @@ router.get('/:id', (req, res, next) => {
 router.post('/', (req, res, next) => {
     try{
         const {body} = req
-        var nextId = Math.max.apply(Math, data.map(function(item) { return item.id; }));
-        body.id = nextId + 1
-        body.rating = Number(body.rating.toFixed(2))
-        data.push(body)
-        res.status(201).json(data)
+        var keys = Object.keys(body)
+        var flag = true
+        if(keys.length == 5){
+            var properties = ['country', 'rating', 'place', 'description', 'activity']
+            keys.forEach(element => {
+                console.log(element)
+                if(!properties.includes(element)){
+                    flag = false
+                }
+            })
+        } else {
+            flag = false
+        }
+
+        console.log(flag)
+        if(flag){
+            var nextId = Math.max.apply(Math, data.map(function(item) { return item.id; }));
+            body.id = nextId + 1
+            body.rating = Number(body.rating.toFixed(2))
+            data.push(body)
+            res.status(201).json(data)
+        }
+        else{
+            res.status(400).json("Check properties of object")
+        }
     } catch(error){
         res.status(404).send('Error! Item not created')
     }
@@ -44,12 +64,17 @@ router.put('/:id', (req, res, next) => {
     try{
         const { params, body } = req
         var place = data.find(item => item.id.toString() === params.id)
-        console.log(place)
-        if(Object.keys(place).length > 0) {
+        var keys = Object.keys(body)
+        var properties = ['country', 'rating', 'place', 'description', 'activity']
+        if(keys.length > 0) {
             var index = data.indexOf(place)
-            data[index] = body
+            keys.forEach(element => {
+                if(properties.includes(element)){
+                    data[index][element] = body[element]
+                }
+            });
             data[index].id = place.id
-            res.status(204)
+            res.status(204).end()
         } else {
             res.status(404).send('Item not found!')
         }
@@ -61,12 +86,15 @@ router.put('/:id', (req, res, next) => {
 //DELETE ROUTE
 router.delete('/:id', (req, res, next) => {
     try {
+        const { params } = req
         console.log(params)
         var place = data.find(item => item.id.toString() === params.id)
+        console.log(place)
         if(Object.keys(place).length > 0) {
             var index = data.indexOf(place)
+            console.log(index)
             data.splice(index, 1)
-            res.status(204)
+            res.status(204).end()
         } else {
             res.status(404).send('Item not found!')
         }
